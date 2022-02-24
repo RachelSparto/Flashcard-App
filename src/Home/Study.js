794dde1dbdfb3757cb6e9b3bc86916bdb8760b3e
RachelSparto <rmbachman2016@hotmail.com>
2022-02-24 11:02:36 -0500

    study section built and functioning

--- src/Home/Study.js
+++ src/Home/Study.js
@@ -1,11 +1,48 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import StudyCardsGood from "./StudyCardsGood";
+import StudyCardsBad from "./StudyCardsBad";
+import { readDeck } from "../utils/api";
 
 function Study() {
+  const [currentDeck, setCurrentDeck] = useState({});
+  const { deckId } = useParams();
+  const { name, cards } = currentDeck;
+
+  useEffect(() => {
+    async function getDeck() {
+      const response = await readDeck(deckId);
+      setCurrentDeck(response);
+    }
+    if (deckId) {
+      getDeck();
+    }
+  }, [deckId]);
+
+  const studySwitch = () => {
+    if (Object.keys(currentDeck).length > 0 && cards.length >= 3) {
+      return <StudyCardsGood />;
+    } else {
+      return <StudyCardsBad />;
+    }
+  };
+
   return (
     <>
-      <Link to="/">Home</Link>
-      <p>Study Stuff</p>
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="breadcrumb-item">
+            <Link to={`/decks/${deckId}`}>{name}</Link>
+          </li>
+          <li className="breadcrumb-item active" aria-current="page">
+            Study
+          </li>
+        </ol>
+      </nav>
+      <div>{studySwitch()}</div>
     </>
   );
 }
